b36ab28de29a7d648838ea29ba8212f78ff7b5a3
skhcodeme <sahilkjan45@gmail.com>
2024-02-16 19:25:31 +0530

    myfirstcode

---
+++ str.js
@@ -0,0 +1,20 @@
+// let a = '$'+(2.5+5);
+// console.log( a)
+
+
+ //let b = '$'+((2095+799)/100);
+ //console.log(b)
+// concatenation
+ let c = 'items ('+(1 + 1)+') : $' + (2095+799)/100;
+ console.log(c)
+
+ let d =  'i\' m learning java script '; console.log(d)
+
+ console.log('Sahil\ntext')
+//Interpolation
+ let ab  = `items (${1+1}): $${(2095+799)/100}` //    this intrapolation 
+ console.log(ab)
+//multiline string
+ let str = `some
+ text`
+ console.log(str)
---
+++ strques.js
@@ -0,0 +1,9 @@
+
+
+let price = '$'+(5+3);
+
+console.log(price)
+
+
+let cost = `$ (${5+3}):`
+console.log(cost)
